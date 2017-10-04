ed6421b005b566771db8f33027cb227c38b88160
mikhail-riabokon <mikhail.riabokon@gmail.com>
2017-10-04 21:37:31 +0300

    test

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
+app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, 'static')));
+
+app.post('/new-request', (req, res) => {
+	console.log('req', req.body);
+
+	res.sendStatus(200);
+});
 
 app.listen(port, () => {
 	console.log(`Server is started at http://localhost:${port}`);
--- static/assets/common/js/main.js
+++ static/assets/common/js/main.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-	$(".sliding-link").click(function(e) {
+$(document).ready(function() {
+	$('.sliding-link').click(function(e) {
 		e.preventDefault();
-		var aid = $(this).attr("href");
-		$('html,body').animate({scrollTop: $(aid).offset().top},'slow');
+		var aid = $(this).attr('href');
+		$('html,body').animate({ scrollTop: $(aid).offset().top }, 'slow');
 	});
 
 	$('.feedback').bxSlider({
@@ -10,7 +10,22 @@ $(document).ready(function(){
 		controls: false,
 		onSliderLoad: function() {
 			$('.feedback').removeClass('feedback-loading');
-		}
+		},
 	});
 
+	$('#request-form').submit(function(event) {
+		event.preventDefault();
+
+		var formData = new FormData(event.currentTarget);
+
+		$.ajax({
+			url: '/new-request',
+			data: formData,
+			processData: false,
+			method: 'POST',
+			success: function() {
+				debugger;
+			},
+		});
+	});
 });
