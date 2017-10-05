59f7a7def16f2e1ce3ea88a18562ceb0ae2f5f3d
Mykhailo Riabokon <mykhailo.riabokon@pipedrive.com>
2017-10-05 11:11:39 +0300

    test

--- static/assets/common/js/main.js
+++ static/assets/common/js/main.js
@@ -1,3 +1,13 @@
+function returnFileSize(number) {
+  if(number < 1024) {
+    return number + 'bytes';
+  } else if(number > 1024 && number < 1048576) {
+    return (number/1024).toFixed(1) + 'KB';
+  } else if(number > 1048576) {
+    return (number/1048576).toFixed(1) + 'MB';
+  }
+}
+
 $(document).ready(function() {
 	$('.sliding-link').click(function(e) {
 		e.preventDefault();
@@ -13,15 +23,40 @@ $(document).ready(function() {
 		},
 	});
 
+	$('#request-form input[name=files]').change(function (event) {
+		var files = event.currentTarget.files;
+		var $parentContainer = $(event.currentTarget).parent();
+		var $filesInfoContainer = $parentContainer.find('.files');
+		var hasFiles = false;
+
+		for (var file in files) {
+			var isFile = files[file] instanceof File;
+
+			if (isFile) {
+				var name = files[file].name;
+				var size = returnFileSize(files[file].size);
+
+				$filesInfoContainer.append('<div class="file"><span class="name">' + name + '</span><span class="size">' + size + '</span></div>');
+				hasFiles = true;
+			}
+		}
+
+		$parentContainer.toggleClass('has-files', hasFiles);
+	});
+
 	$('#request-form').submit(function(event) {
 		event.preventDefault();
 
-		var formData = new FormData(event.currentTarget);
+        var formData = $(this).serializeArray();
+        var formDataToSend = {};
+
+        for (var i = 0; i < formData.length; i++) {
+            formDataToSend[formData[i].name] = formData[i].value || '';
+        }
 
 		$.ajax({
 			url: '/new-request',
-			data: formData,
-			processData: false,
+			data: formDataToSend,
 			method: 'POST',
 			success: function() {
 				debugger;
